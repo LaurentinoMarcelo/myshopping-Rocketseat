1041df24c777e4bd9f636818cfefcaaa27bbd6d8
Marcelo Laurentino <marcelolaurentino56@gmail.com>
2022-02-02 21:10:34 -0300

    Criando busca ordenada firebase

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,38 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { FlatList } from "react-native";
 
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
+import { styles } from "./styles";
+import { Product, ProductProps } from "../Product";
 
-import { shoppingListExample } from '../../utils/shopping.list.data';
- import firestore from '@react-native-firebase/firestore'
+import firestore from "@react-native-firebase/firestore";
 
 export function ShoppingList() {
   const [products, setProducts] = useState<ProductProps[]>([]);
 
   useEffect(() => {
-    firestore()
-    .collection('product')
-    .get()
-    .then(response => {
-      const data = response.docs.map(doc => {
-        return{
-          id: doc.id,
-          ...doc.data()
-        }
-      }) as ProductProps[];
-      setProducts(data);
-      console.log('sucesso carregamento');
-      
-    })
-    .catch(error => console.log(error));
-  }, [])
-
+    const subscribe = firestore()
+      .collection("product")
+      .where('quantity', '==', 1)
+      .onSnapshot((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+        setProducts(data);
+      });
+    return () => subscribe();
+  }, []);
 
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
